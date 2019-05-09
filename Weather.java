79c4cca41664dfdb2bf8eeb518a2a03b6327d0d3
Oliver Wales <33153339+OliverWales@users.noreply.github.com>
2019-05-09 15:42:04 +0100

    Switched from Strings to JsonObjects
    Switched from Strings to JsonObjects, enjoy x

--- Weather.java
+++ Weather.java
@@ -9,13 +9,17 @@ public class Weather {
     private static final double ZERO_Celcius = 273.15;
 
     // Takes a location string of form "Cambridge,UK", returns json string
-    public static String getCurrentWeatherString(String location) {
+    public static JsonObject getCurrentWeatherObject(String location) {
         String url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + API_KEY;
 
         try {
             URLConnection connection = (new URL(url)).openConnection();
             BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            return br.readLine();
+
+            Gson gson = new Gson();
+            JsonObject jsonObject = gson.fromJson(br.readLine(), JsonObject.class);
+
+            return jsonObject;
 
         } catch (Exception e) {
             System.out.println("Error getting weather data: " + e);
@@ -25,38 +29,42 @@ public class Weather {
     }
 
     // Takes json string, returns temperature
-    public static double getTemp(String json) {
-        Gson gson = new Gson();
-        JsonObject gsonObject = gson.fromJson(json, JsonObject.class);
-        JsonElement main = gsonObject.get("main");
+    public static double getTemp(JsonObject jsonObject) {
+        JsonElement main = jsonObject.get("main");
         double temp = main.getAsJsonObject().get("temp").getAsDouble() - ZERO_Celcius;
         return Math.round(temp * 10)/10.0;
     }
 
     // Takes json string, returns weather type
-    public static String getType(String json) {
-        Gson gson = new Gson();
-        JsonObject gsonObject = gson.fromJson(json, JsonObject.class);
-        JsonArray weather = gsonObject.getAsJsonArray("weather");
+    public static String getType(JsonObject jsonObject) {
+        JsonArray weather = jsonObject.getAsJsonArray("weather");
         return weather.get(0).getAsJsonObject().get("main").getAsString();
+    }
+
+    // Takes json string, returns weather type
+    public static String getDesc(JsonObject jsonObject) {
+        JsonArray weather = jsonObject.getAsJsonArray("weather");
+        return weather.get(0).getAsJsonObject().get("description").getAsString();
     }
 
     // Example usage
     public static final void main(String[] args) {
-        Weather weather = new Weather();
         final String LOCATION1 = "Cambridge,UK";
-        final String LOCATION2 = "London,UK";
+        final String LOCATION2 = "Girton,UK";
+        final String LOCATION3 = "London,UK";
 
         try {
-            String json1 = getCurrentWeatherString(LOCATION1);
-            String json2 = getCurrentWeatherString(LOCATION2);
+            JsonObject json1 = getCurrentWeatherObject(LOCATION1);
+            JsonObject json2 = getCurrentWeatherObject(LOCATION2);
+            JsonObject json3 = getCurrentWeatherObject(LOCATION3);
 
-            System.out.println(getTemp(json1)+ "°C, " + getType(json1) + " in " + LOCATION1); //LOCATION1.substring(0, LOCATION1.length() - 3));
-            System.out.println(getTemp(json2)+ "°C, " + getType(json2) + " in " + LOCATION2); //LOCATION2.substring(0, LOCATION2.length() - 3));
+            System.out.println(json1);
+
+            System.out.println(getTemp(json1)+ "°C, " + getType(json1) + " (" + getDesc(json1) + ") in " + LOCATION1.substring(0, LOCATION1.length() - 3));
+            System.out.println(getTemp(json2)+ "°C, " + getType(json2) + " (" + getDesc(json2) + ") in "  + LOCATION2.substring(0, LOCATION2.length() - 3));
+            System.out.println(getTemp(json3)+ "°C, " + getType(json3) + " (" + getDesc(json3) + ") in "  + LOCATION3.substring(0, LOCATION2.length() - 3));
         } catch (Exception e) {
             System.out.println("Error connecting to URL. " + e);
         }
-
-
     }
 }
