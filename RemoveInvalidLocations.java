f635848863997b27aeada22449ba97c74eeb37d5
Archie Wheeler <archwheeler@gmail.com>
2019-05-14 15:38:44 +0100

    create a tool to remove locations from citylist.txt which aren't supported by the weather API

---
+++ RemoveInvalidLocations.java
@@ -0,0 +1,25 @@
+import WeatherApp.Weather;
+
+import java.io.*;
+
+public class RemoveInvalidLocations {
+    public static void main(String[] args) throws IOException {
+        File fout = new File("citylistFixed.txt");
+        FileOutputStream fos = new FileOutputStream(fout);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "/home/archie/Documents/weather-app/citylist.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+                if (Weather.getCurrentWeatherObject(line) != null) bw.write(line + "\n");
+                line = reader.readLine();
+            }
+            reader.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
