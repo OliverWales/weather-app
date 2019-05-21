ab470fde63590e78738894a7480b15524df9643e
Archie Wheeler <archwheeler@gmail.com>
2019-05-21 14:18:05 +0100

    add lots of comments to SearchBar classes

--- src/WeatherApp/CitySearchBar.java
+++ src/WeatherApp/CitySearchBar.java
@@ -10,25 +10,30 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+// A wrapper class for JComboBox that automatically sets various settings and handles selections from drop down list.
+// The drop down list contains a list of locations stored in citylist.txt
+// The user's search is autocompleted to prevent typing in entire name.
 public class CitySearchBar extends JComboBox implements ActionListener {
-    static final String cities = "/home/archie/Documents/weather-app/data/citylist.txt";
-    private List<String> locations;
-    private String selectedLocation;
+    static final String cities = "/home/archie/Documents/weather-app/data/citylist.txt"; // file containing cities
+    private List<String> locations; // city file read into a List
+    private String selectedLocation; // currently selected city
 
     public CitySearchBar() throws IOException {
-        super((Files.readAllLines(Paths.get(cities))).toArray());
-        locations = Files.readAllLines(Paths.get(cities));
-        selectedLocation = null;
-        AutoCompletion.enable(this);
-        this.setEditable(true);
-        this.setSelectedIndex(-1);
-        this.addActionListener(this);
+        super((Files.readAllLines(Paths.get(cities))).toArray()); // create a new JComboBox
+        locations = Files.readAllLines(Paths.get(cities)); // read in city file
+        selectedLocation = null; // nothing is selected initially
+        AutoCompletion.enable(this); // credits: http://www.orbital-computer.de/JComboBox/
+        this.setEditable(true); // user can type into the search bar
+        this.setSelectedIndex(-1); // search bar is empty initially
+        this.addActionListener(this); // call this.actionPerformed() when a city is selected
     }
 
+    // when a city is selected, update the currently selected location
     public void actionPerformed(ActionEvent e) {
         selectedLocation = (String) this.getSelectedItem();
     }
 
+    // return the selected location
     public String getSelected() {
         return selectedLocation;
     }
--- src/WeatherApp/HistorySearchBar.java
+++ src/WeatherApp/HistorySearchBar.java
@@ -10,23 +10,28 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+// A wrapper class for JComboBox that automatically sets various settings and handles selections from drop down list.
+// The drop down list contains a list of the user's previous searches.
+// This list is stored in a file which is updated upon entering a new search.
 public class HistorySearchBar extends JComboBox implements ActionListener {
-    static final String history = "/home/archie/Documents/weather-app/data/history.txt";
-    private List<String> locations;
-    private String selectedLocation;
+    static final String history = "/home/archie/Documents/weather-app/data/history.txt"; // file containing previous searches
+    private List<String> locations; // previous searches loaded into a List
+    private String selectedLocation; // currently selected city
 
     public HistorySearchBar() throws IOException {
-        super((Files.readAllLines(Paths.get(history))).toArray());
-        locations = Files.readAllLines(Paths.get(history));
-        selectedLocation = null;
-        this.setEditable(true);
-        this.setSelectedIndex(-1);
-        this.addActionListener(this);
+        super((Files.readAllLines(Paths.get(history))).toArray()); // create a new JComboBox
+        locations = Files.readAllLines(Paths.get(history)); // read in history file
+        selectedLocation = null; // nothing is selected initially
+        this.setEditable(true); // user can type into the search bar
+        this.setSelectedIndex(-1); // search bar is empty initially
+        this.addActionListener(this); // call this.actionPerformed() when a city is selected
     }
 
     public void actionPerformed(ActionEvent e) {
-        selectedLocation = (String) this.getSelectedItem();
+        selectedLocation = (String) this.getSelectedItem(); // update the selected location
+        // check if user hasn't searched for the selected location before
         if (!locations.contains(selectedLocation)) {
+            // if so, update our history file (append new search to the end) and update our history List
             try {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(history, true));
                 writer.write(selectedLocation + '\n');
