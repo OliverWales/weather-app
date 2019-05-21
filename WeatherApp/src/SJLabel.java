a6edb446fed835c40aad3e2801c037c8de512a95
Whiles9799 <t.while@live.co.uk>
2019-05-21 15:14:08 +0100

    Add files via upload

---
+++ WeatherApp/src/SJLabel.java
@@ -0,0 +1,19 @@
+package WeatherApp;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.Font.BOLD;
+
+public class SJLabel extends JLabel {
+    Font font;
+    public SJLabel(String text){
+        super(text);
+        font = new Font("Hiragino Sans GB", BOLD,20);
+        setFont(font);
+    }
+    public void setTextSize(int size){
+        font = new Font(font.getFontName(), BOLD, size);
+
+    }
+}
